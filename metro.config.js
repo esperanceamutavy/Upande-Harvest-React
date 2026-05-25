fdb380c2650e36ae0d91b99216580d40890d64ab
esperanceamutavy <esperanceamutavy@gmail.com>
2026-05-25 10:21:42 +0300

    Phase 2 fix: disable Tamagui babel + metro compiler plugins (runtime config only)

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withTamagui } = require('@tamagui/metro-plugin');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withTamagui(config, {
-  components: ['tamagui'],
-  config: './tamagui.config.ts',
-});
+module.exports = config;
